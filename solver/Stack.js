ed0319693adff6f3b70241b19302bf465e60f9f0
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-11-05 13:42:47 +0100

    Can show correct path and saves the full "solve stack" (and displays!). NEXT: Animate the discovery

---
+++ solver/Stack.js
@@ -0,0 +1,74 @@
+// Implemented with singly linked list
+// Last in first out
+export default class Stack {
+  tail = null;
+
+  push(data) {
+    const newNode = new Node(data);
+    newNode.next = this.tail;
+    this.tail = newNode;
+  }
+
+  pop() {
+    const prev = this.tail;
+    if (prev){
+      this.tail = prev.next;
+
+    }
+
+    return prev;
+  }
+
+  peek() {
+    return this.tail;
+  }
+
+  size() {
+    let size = 0;
+    let current = this.tail;
+
+    while (current) {
+      size++;
+      current = current.next;
+    }
+
+    return size;
+  }
+
+  get(index) {
+    let currentIndex = 0;
+    let current = this.tail;
+
+    while (currentIndex < index) {
+      currentIndex++;
+      if (current == null){
+        return;
+      }
+      current = current.next;
+    }
+
+    return current;
+  }
+
+  dumpList(){
+    let current = this.tail
+    while (current){
+      console.log(current);
+      
+      current = current.next
+    }
+  }
+}
+
+class Node {
+  data = null;
+  next = null;
+  constructor(data = null, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+
+  setNext(newNext) {
+    this.next = newNext;
+  }
+}
--- solver/solver.js
+++ solver/solver.js
@@ -1,50 +1,164 @@
+import Stack from "./Stack.js";
 
-window.addEventListener("load",start)
+window.addEventListener("load", start);
 
-const mazeObj = {
-    "rows": 4,
-    "cols": 4,
-    "start": {"row": 0, "col": 0},
-    "goal": {"row": 2, "col": 3},
-    "maze":
-    [
-      [{"row":0,"col":0,"north":true,"east":true,"west":true,"south":false},
-       {"row":0,"col":1,"north":true,"east":false,"west":true,"south":false},
-       {"row":0,"col":2,"north":true,"east":false,"west":false,"south":true},
-       {"row":0,"col":3,"north":true,"east":true,"west":false,"south":false}],
-      [{"row":1,"col":0,"north":false,"east":false,"west":true,"south":true},
-       {"row":1,"col":1,"north":false,"east":true,"west":false,"south":true},
-       {"row":1,"col":2,"north":true,"east":false,"west":true,"south":false},
-       {"row":1,"col":3,"north":false,"east":true,"west":false,"south":true}],
-      [{"row":2,"col":0,"north":true,"east":false,"west":true,"south":false},
-       {"row":2,"col":1,"north":true,"east":true,"west":false,"south":true},
-       {"row":2,"col":2,"north":false,"east":true,"west":true,"south":false},
-       {"row":2,"col":3,"north":true,"east":true,"west":true,"south":false}],
-      [{"row":3,"col":0,"north":false,"east":false,"west":true,"south":true},
-       {"row":3,"col":1,"north":true,"east":false,"west":false,"south":true},
-       {"row":3,"col":2,"north":false,"east":false,"west":false,"south":true},
-       {"row":3,"col":3,"north":false,"east":true,"west":false,"south":true}]
-    ]
+async function start() {
+  const mazeInfo = await readJSON();
+  console.log("Maze:", mazeInfo);
+  createMaze(mazeInfo);
+  // recursiveSolver(maze, { row: 2, col: 0 });
+  const solveStack = iterativeSolver(mazeInfo, mazeInfo.maze[mazeInfo.start.row][mazeInfo.start.col]);
+  console.log("solveStack:", solveStack);
+  if (solveStack) {
+    solveStack.dumpList();
   }
-
-function start(){
-    console.log("Script running");
-    // console.log("Maze:",mazeObj);
-    createMaze()
 }
 
-function createMaze(){
-    const container = document.querySelector("#maze")
-    for (const row of mazeObj.maze) {
-        for (const tile of row) {
-            const node = document.createElement("div")
-            node.classList.add("cell")
-            for (const key in tile) {
-                if (tile[key] && key != "row" && key != "col") {
-                    node.classList.add(key)
-                }
-            }
-            container.appendChild(node)
+async function readJSON(filePath = "./maze.json") {
+  return await fetch(filePath).then(response => response.json());
+}
+
+function createMaze(mazeObj) {
+  const container = document.querySelector("#maze");
+  const totalCols = mazeObj.cols;
+  for (const row of mazeObj.maze) {
+    for (const tile of row) {
+      const node = document.createElement("div");
+
+      if (tile.row == mazeObj.start.row && tile.col == mazeObj.start.col) {
+        node.classList.add("start");
+      } else if (tile.row == mazeObj.goal.row && tile.col == mazeObj.goal.col) {
+        node.classList.add("goal");
+      }
+      node.classList.add("cell");
+      for (const key in tile) {
+        if (tile[key] && key != "row" && key != "col") {
+          node.classList.add(key);
         }
+      }
+
+      node.dataset.index = tile.row * totalCols + tile.col;
+
+      node.style.setProperty("--CELL-SIZE", 40 / mazeObj.rows + "em");
+      container.appendChild(node);
     }
+  }
+}
+
+function iterativeSolver(mazeObj, currentTile) {
+  const tileStack = new Stack();
+  const branchStack = new Stack();
+  console.log("The stack:", tileStack);
+  console.log("THe tile:", currentTile);
+  tileStack.push(currentTile);
+
+  let iterations = 0;
+  while (tileStack.peek()) {
+    let tile = tileStack.peek().data;
+    console.log("Tile:", tile);
+    // if (!tile.discovered) {
+    markAsDiscovered(mazeObj, tile);
+    const validNeighbors = getNeighbourCoords(mazeObj, tile);
+    // console.log("tile stack:", tileStack.peek());
+    console.log("Neighbors:", validNeighbors);
+    if (validNeighbors.length > 1) {
+      console.log("At branch");
+      branchStack.push(tile);
+    }
+    for (const neighbor of validNeighbors) {
+      console.log(neighbor);
+
+      tileStack.push(neighbor);
+      if (neighbor.row == mazeObj.goal.row && neighbor.col == mazeObj.goal.col) {
+        console.log("Found the goal!");
+        markAsCorrectPath(mazeObj, tileStack);
+        return tileStack;
+      }
+    }
+    if (validNeighbors.length == 0 && branchStack.peek()) {
+      console.log("At dead end:", tile);
+      // Mark tiles as discovered until we find the last branch
+      const lastBranch = branchStack.pop().data;
+      console.log("last branch:", lastBranch);
+
+      while (lastBranch.row != tileStack.peek().data.row || lastBranch.col != tileStack.peek().data.col) {
+        // while (lastBranch.row != tile.row || lastBranch.col != tile.col) {
+        markAsDeadEnd(mazeObj, tile);
+        tile = tileStack.pop().data;
+      }
+      console.log("Left while loop at:", tile);
+      // tile = tileStack.peek().data;
+
+      // }
+    }
+    iterations++;
+    if (iterations > 50) {
+      break;
+    }
+  }
+
+  console.log("final tile stack:");
+  tileStack.dumpList();
+}
+
+function markAsCorrectPath(mazeObj, stack) {
+  // Leave the goal tile out of the stack
+  stack.pop()
+  // And stop before we reach the start tile
+  while (stack.peek().next) {
+    const tile = stack.pop().data;
+    const coordId = tile.row * mazeObj.cols + tile.col;
+    const correctTile = document.querySelector(`[data-index="${coordId}"]`);
+    correctTile.classList.add("correctPath");
+  }
+}
+
+function recursiveSolver(mazeObj, currentTile) {
+  markAsDiscovered(mazeObj, currentTile);
+  const validNeighbours = getNeighbourCoords(mazeObj, currentTile);
+  console.log("Valid neighbors:", validNeighbours);
+  for (const neighbor of validNeighbours) {
+    if (neighbor.row == mazeObj.goal.row && neighbor.col == mazeObj.goal.col) {
+      console.log("Found the goal!");
+      return neighbor;
+    }
+    if (!neighbor.discovered) {
+      recursiveSolver(mazeObj, neighbor);
+    }
+  }
+}
+
+function markAsDiscovered(mazeObj, coordinates) {
+  coordinates.discovered = true;
+  const coordId = coordinates.row * mazeObj.cols + coordinates.col;
+  const correctTile = document.querySelector(`[data-index="${coordId}"]`);
+  correctTile.classList.add("discovered");
+}
+
+function markAsDeadEnd(mazeObj, coordinates) {
+  coordinates.discovered = true;
+  const coordId = coordinates.row * mazeObj.cols + coordinates.col;
+  const correctTile = document.querySelector(`[data-index="${coordId}"]`);
+  correctTile.classList.remove("discovered");
+  correctTile.classList.add("deadEnd");
+}
+
+function getNeighbourCoords(mazeObj, coordinates) {
+  const validCoords = [];
+  const correctTile = mazeObj.maze[coordinates.row][coordinates.col];
+  const m = mazeObj.maze;
+  if (!correctTile["south"] && !m[correctTile.row + 1][correctTile.col].discovered) {
+    validCoords.push(m[correctTile.row + 1][correctTile.col]);
+  }
+  if (!correctTile["east"] && !m[correctTile.row][correctTile.col + 1].discovered) {
+    validCoords.push(m[correctTile.row][correctTile.col + 1]);
+  }
+  if (!correctTile["west"] && !m[correctTile.row][correctTile.col - 1].discovered) {
+    validCoords.push(m[correctTile.row][correctTile.col - 1]);
+  }
+  if (!correctTile["north"] && !m[correctTile.row - 1][correctTile.col].discovered) {
+    validCoords.push(m[correctTile.row - 1][correctTile.col]);
+  }
+
+  return validCoords;
 }
